2c01eb882c2e5b7537f4dc76ec70aee6e3486427
johnsonawa29-driod <johnsonawa29@gmail.com>
2025-11-08 15:57:40 +0100

    yes

---
+++ 7-multi_c.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const [, , arg] = process.argv;
+const x = parseInt(arg);
+
+if (isNaN(x)) console.log('Missing number of occurrences');
+else for (let i = 0; i < x; i++) console.log('C is fun');
